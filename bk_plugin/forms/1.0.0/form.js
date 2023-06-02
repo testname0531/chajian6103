9862a4f3e55d1cb950dd8373c8a2fd4f7488bf82
testname0531 <v_jiaqigeng@tencent.com>
2023-06-02 17:31:29 +0800

    init repo

---
+++ bk_plugin/forms/1.0.0/form.js
@@ -0,0 +1,12 @@
+(function () {
+    $.atoms["app_code03"] = [
+        {
+            "type": "input",
+            "attrs": {
+                "name": "Hello",
+                "hookable": true
+            },
+            "tag_code": "hello"
+        }
+    ]
+})();
